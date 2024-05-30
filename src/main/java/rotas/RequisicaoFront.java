8378051bf1c1b2121323892f4d80cb1736a78213
GuedesThi <thiagodesignguedes@gmail.com>
2024-05-30 16:19:31 -0300

    agora vai pela mor de Deus

---
+++ src/main/java/rotas/RequisicaoFront.java
@@ -0,0 +1,13 @@
+package rotas;
+
+public class RequisicaoFront {
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
